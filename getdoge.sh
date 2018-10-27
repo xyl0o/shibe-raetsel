01a80fab3312d3c24ded1b20f51af192551f464d
xyl0o <xyl0o@posteo.de>
2018-10-27 18:11:50 +0200

    Basic clean up

--- getdoge.sh
+++ getdoge.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O dodge.png http://vignette2.wikia.nocookie.net/creepypasta/images/0/05/Doge.png/revision/latest?cb=20131121015552
+curl --output dodge.png http://vignette2.wikia.nocookie.net/creepypasta/images/0/05/Doge.png/revision/latest?cb=20131121015552
--- src/conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf(t)
-	t.window.width = 1024
-	t.window.height = 768
-	t.version = "0.10.0"
-	t.window.title = "shibe-raetsel"
-	t.window.vsync = true
-end
