fdbb672c8b795b0c4a89872d4535b6203db8ed8f
xyl0o <xyl0o@posteo.de>
2018-10-27 20:33:00 +0200

    Move to basic package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='shibe_raetsel',
+    version='0.1',
+    package_dir={'': 'src'},
+    packages=find_packages(),
+)
